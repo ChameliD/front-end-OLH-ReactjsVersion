3a91db58959b652d7145ae725bcc4fdb8bb258f7
Chameli <Chameli@example.com>
2022-05-25 15:52:47 +0530

    add a header

---
+++ src/Components/Header/HeaderComponents.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class HeaderComponents extends Component {
+  render() {
+    return (
+      <div>HeaderComponents</div>
+    )
+  }
+}
+
+export default HeaderComponents
